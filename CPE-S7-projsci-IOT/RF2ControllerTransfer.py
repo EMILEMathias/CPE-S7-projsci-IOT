958f84775e94863216f89d0f16c7af3238cb659f
EMILEMathias <mathias.emile@gmail.com>
2023-01-11 15:52:04 +0100

    fix:noice

--- CPE-S7-projsci-IOT/RF2ControllerTransfer.py
+++ CPE-S7-projsci-IOT/RF2ControllerTransfer.py
@@ -23,7 +23,7 @@ def findSerialPort():
         ports = ''
    for port in ports:
         try:
-            SerialPort = "/dev/ttyACM2"
+            SerialPort = "/dev/ttyACM0"
             # SerialPort = port 
         except (OSError, serial.SerialException):
             pass
